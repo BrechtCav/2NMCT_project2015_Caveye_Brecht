2a916020ac645c2bb07929127adf2f71a1a1925b
BrechtCav <brecht.caveye@outlook.com>
2015-05-21 14:16:49 +0200

    Update 3

--- app/src/main/java/nmct/howest/be/switchshop/loader/ShopInfo.java
+++ app/src/main/java/nmct/howest/be/switchshop/loader/ShopInfo.java
@@ -1,8 +1,12 @@
 package nmct.howest.be.switchshop.loader;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by BrechtCaveye on 20/05/2015.
  */
 public final class ShopInfo {
-    public static final String COLUMN_SHOP_NAAM
+    public interface ShopColumns extends BaseColumns{
+        public static final String COLUMN_SHOP_NAAM = "shop_naam";
+    }
 }
